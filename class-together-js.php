191a0248ef1b927985bc7a7c9a5af4a433ee0672
Barry Ceelen <b@rryceelen.com>
2016-04-11 11:09:54 +0200

    Whitespace

--- class-together-js.php
+++ class-together-js.php
@@ -202,7 +202,7 @@ class TogetherJS {
 		} elseif ( 'gravatar_default' === $default ) {
 			$default = '';
 		} elseif ( strpos( $default, 'http://' ) === 0 ) {
-				$default = add_query_arg( 's', $size, $default );
+			$default = add_query_arg( 's', $size, $default );
 		}
 
 		$out  = "$host/avatar/";
